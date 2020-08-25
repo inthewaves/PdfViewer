b4ff2a1f0f81a6b09d8dedb83a55dadf05c44b7b
inthewaves <26474149+inthewaves@users.noreply.github.com>
2020-08-25 10:52:25 -0700

    use async functions to reason about promises

--- app/src/main/java/org/grapheneos/pdfviewer/model/OutlineEntry.kt
+++ app/src/main/java/org/grapheneos/pdfviewer/model/OutlineEntry.kt
@@ -1,5 +1,5 @@
 package org.grapheneos.pdfviewer.model
 
-class OutlineEntry(val title: String, val children: List<OutlineEntry>) {
+class OutlineEntry(val title: String, val pageNumber: Int, val children: List<OutlineEntry>) {
 
 }
